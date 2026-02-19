a0b0362e9053cccc732c79b69a7a64c40760e20e
pol-dev-shinroo <manus.dream.language@gmail.com>
2026-02-20 03:57:54 +0900

    added static export config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default function Home() {
     <div className="max-w-3xl mx-auto py-20 px-4">
       <section className="text-center mb-10">
         <h1 className="text-5xl font-black mb-6 tracking-tight text-slate-900">
-          Crop Your PDF. <span className="text-blue-600">Instantly.</span>
+          Crop Your PDF.{" "}
+          <span className="text-blue-600">Instantly.(안녕 예원!)</span>
         </h1>
         <p className="text-lg text-slate-500 font-medium">
           The safest way for students to split assignments. <br /> 100% private,
