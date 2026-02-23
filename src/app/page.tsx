fa3d77079961785a8cbcca53cf0d23484d794ed2
pol-dev-shinroo <manus.dream.language@gmail.com>
2026-02-23 19:56:35 +0900

    polishing up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,11 @@ export default function Home() {
   return (
     <div className="min-h-screen pt-28">
       <Header activeTab={activeTab} onTabChange={setActiveTab} />
-      
+
       <main className="max-w-3xl mx-auto px-4 pb-20">
         <section className="text-center mb-12">
           <h1 className="text-5xl font-black mb-6 tracking-tight text-[#355872]">
             Do School Work.{" "}
-            <span className="text-[#7AAACE] font-extrabold italic">Better.(안녕 예원!)</span>
           </h1>
           <p className="text-lg text-[#355872]/70 font-bold">
             The ultimate toolbox for PDF assignments. <br /> 100% private,
